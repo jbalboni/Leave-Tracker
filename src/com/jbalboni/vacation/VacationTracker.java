621ef4dc7c4e2c0eb34b97503a9c0d48727011e6
Jeff Balboni <jbalboni@gmail.com>
2011-12-19 20:16:58 -0500

    Can choose as of date for vacation time

--- src/com/jbalboni/vacation/VacationTracker.java
+++ src/com/jbalboni/vacation/VacationTracker.java
@@ -10,24 +10,21 @@ public class VacationTracker
     private static final int DAYS_IN_WEEK = 7;
     
     private LocalDate startDate;
-    private LocalDate currentDate;
     private float hoursUsed;
     private int daysPerYear;
     
     public VacationTracker() {
-       this.currentDate = new LocalDate();
     }
     public VacationTracker(LocalDate startDate, float hoursUsed, int daysPerYear) {
         this.startDate = startDate;
         this.hoursUsed = hoursUsed;
         this.daysPerYear = daysPerYear;
-        this.currentDate = new LocalDate();
      }
-    public float calculateHours() {
-        Days days = Days.daysBetween(startDate, currentDate);
+    public float calculateHours(LocalDate asOfDate) {
+        Days days = Days.daysBetween(startDate, asOfDate);
         float hoursPerYear = daysPerYear * HOURS_IN_DAY;
-        float vacationDays = (days.getDays() * ((hoursPerYear / WEEKS_IN_YEAR) / DAYS_IN_WEEK) - hoursUsed) / HOURS_IN_DAY;
-        return vacationDays;
+        float vacationHours = (days.getDays() * ((hoursPerYear / WEEKS_IN_YEAR) / DAYS_IN_WEEK) - hoursUsed);
+        return vacationHours;
     }
     public void setStartDate(LocalDate startDate)
     {
--- src/com/jbalboni/vacation/VacationTrackerActivity.java
+++ src/com/jbalboni/vacation/VacationTrackerActivity.java
@@ -1,21 +1,34 @@
 package com.jbalboni.vacation;
 
+import org.joda.time.LocalDate;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
 import com.markupartist.android.widget.ActionBar;
 import com.markupartist.android.widget.ActionBar.Action;
 import com.markupartist.android.widget.ActionBar.IntentAction;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class VacationTrackerActivity extends Activity {
     private VacationTracker vacationTracker;
+    private LocalDate asOfDate = new LocalDate();
+    
+    private Button asOfDatePicker;
+    static final int DATE_DIALOG_ID = 0;
+    DateTimeFormatter fmt = DateTimeFormat.forPattern("MM/dd/yyyy");
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,15 +40,42 @@ public class VacationTrackerActivity extends Activity {
         
         vacationTracker = VacationStateManager.createVacationTracker(prefs,this);
         
-        TextView daysAvailable = (TextView)findViewById(R.id.daysAvailable);
-        daysAvailable.setText(String.format("%s %.2f",getString(R.string.days_avail),vacationTracker.calculateHours()));
+        TextView daysAvailable = (TextView)findViewById(R.id.hoursAvailable);
+        daysAvailable.setText(String.format("%s %.2f",getString(R.string.hours_avail),vacationTracker.calculateHours(asOfDate)));
         
         ActionBar actionBar = (ActionBar) findViewById(R.id.actionbar);
         //actionBar.setHomeAction(new IntentAction(this, VacationTrackerActivity.createIntent(this), R.drawable.ic_title_home_default));
         actionBar.addAction(new ToastAction());
         final Action settingsAction = new IntentAction(this, new Intent(this, SettingsActivity.class), R.drawable.ic_action_settings);
         actionBar.addAction(settingsAction);
+        
+        asOfDatePicker = (Button) findViewById(R.id.changeAsOfDate);
+
+        // add a click listener to the button
+        asOfDatePicker.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                showDialog(DATE_DIALOG_ID);
+            }
+        });
     }
+    
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+        case DATE_DIALOG_ID:
+            return new DatePickerDialog(this,
+                    mDateSetListener, asOfDate.getYear(), asOfDate.getMonthOfYear()-1, asOfDate.getDayOfMonth());
+        }
+        return null;
+    }
+    
+    private DatePickerDialog.OnDateSetListener mDateSetListener =
+        new DatePickerDialog.OnDateSetListener() {
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                asOfDate = new LocalDate(year,monthOfYear+1,dayOfMonth);
+                updateDisplay();
+            }
+        };
     
     @Override
     protected void onResume() {
@@ -43,8 +83,20 @@ public class VacationTrackerActivity extends Activity {
         
         vacationTracker = VacationStateManager.createVacationTracker(PreferenceManager.getDefaultSharedPreferences(this),this);
         
-        TextView daysAvailable = (TextView)findViewById(R.id.daysAvailable);
-        daysAvailable.setText(String.format("%.2f",vacationTracker.calculateHours()));
+        updateDisplay();
+    }
+
+    private void updateDisplay()
+    {
+        TextView daysAvailable = (TextView)findViewById(R.id.hoursAvailable);
+        daysAvailable.setText(String.format("%.2f",vacationTracker.calculateHours(asOfDate)));
+        TextView asOfDateTextView = (TextView) findViewById(R.id.asOfDateDesc);
+        if (asOfDate.compareTo(new LocalDate()) != 0) {
+            asOfDateTextView.setText(fmt.print(asOfDate));
+        }
+        else {
+            asOfDateTextView.setText(getString(R.string.default_as_of_date));
+        }
     }
     
     public static Intent createIntent(Context context) {
