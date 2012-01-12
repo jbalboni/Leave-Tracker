d0fdeb26e7ed749d00552b199cd49a54adc5954e
Jeff Balboni <jbalboni@gmail.com>
2012-01-11 23:13:23 -0500

    view pager, categories, fully working

--- src/com/jbalboni/vacation/LeaveStateManager.java
+++ src/com/jbalboni/vacation/LeaveStateManager.java
@@ -1,33 +1,46 @@
 package com.jbalboni.vacation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.joda.time.LocalDate;
 import org.joda.time.format.DateTimeFormatter;
 import org.joda.time.format.ISODateTimeFormat;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 
 public final class LeaveStateManager
 {
     private static final DateTimeFormatter fmt = ISODateTimeFormat.localDateParser();
-    public static VacationTracker createVacationTracker(SharedPreferences prefs)
+    public static VacationTracker createVacationTracker(SharedPreferences prefs, String categoryPrefix)
     {
-        String startDateStr = prefs.getString("startdate", "2011-05-02");
-        LocalDate startDate = fmt.parseLocalDate(startDateStr);
-        Float hoursUsed = Float.parseFloat(prefs.getString("hoursUsed", "0"));
-        float hoursPerYear = Float.parseFloat(prefs.getString("hoursPerYear", "80"));
-        float initialHours = Float.parseFloat(prefs.getString("initialHours", "0"));
+        String startDateStr = prefs.getString("startDate",null);
+        String leaveInterval = prefs.getString("leaveInterval", "Day");
+        LocalDate startDate = startDateStr == null ? new LocalDate() : fmt.parseLocalDate(startDateStr);
+        Float hoursUsed = Float.parseFloat(prefs.getString(categoryPrefix+"hoursUsed", "0"));
+        float hoursPerYear = Float.parseFloat(prefs.getString(categoryPrefix+"hoursPerYear", "80"));
+        float initialHours = Float.parseFloat(prefs.getString(categoryPrefix+"initialHours", "0"));
         
-        return new VacationTracker(startDate,hoursUsed,hoursPerYear,initialHours);
+        return new VacationTracker(startDate,hoursUsed,hoursPerYear,initialHours,leaveInterval);
     }
-    public static void saveVacationTracker(VacationTracker vacationTracker, SharedPreferences prefs)
+    public static void saveVacationTracker(VacationTracker vacationTracker, SharedPreferences prefs, String categoryPrefix)
     {
         Editor prefsEditor = prefs.edit();
-        prefsEditor.putString("hoursUsed", Float.toString(vacationTracker.getHoursUsed()));
-        prefsEditor.putString("hoursPerYear", Float.toString(vacationTracker.getHoursPerYear()));
-        prefsEditor.putString("intialHours", Float.toString(vacationTracker.getInitialHours()));
+        prefsEditor.putString(categoryPrefix+"hoursUsed", Float.toString(vacationTracker.getHoursUsed()));
+        prefsEditor.putString(categoryPrefix+"hoursPerYear", Float.toString(vacationTracker.getHoursPerYear()));
+        prefsEditor.putString(categoryPrefix+"intialHours", Float.toString(vacationTracker.getInitialHours()));
         prefsEditor.putString("startDate", String.format("%4d-%2d-%2d",vacationTracker.getStartDate().getYear(),vacationTracker.getStartDate().getMonthOfYear()
                 ,vacationTracker.getStartDate().getDayOfMonth()));
         prefsEditor.commit();
+    }
+    
+    public static List<String> getTitles(SharedPreferences prefs, Context ctxt) {
+        List<String> titles = new ArrayList<String>();
+        titles.add(prefs.getString(LeaveCategory.LEFT.getPrefix()+"title", ctxt.getString(R.string.default_left_name)));
+        titles.add(prefs.getString(LeaveCategory.CENTER.getPrefix()+"title", ctxt.getString(R.string.default_center_name)));
+        titles.add(prefs.getString(LeaveCategory.RIGHT.getPrefix()+"title", ctxt.getString(R.string.default_right_name)));
+        return titles;
     }
 }
--- src/com/jbalboni/vacation/VacationTracker.java
+++ src/com/jbalboni/vacation/VacationTracker.java
@@ -2,28 +2,46 @@ package com.jbalboni.vacation;
 
 import org.joda.time.Days;
 import org.joda.time.LocalDate;
+import org.joda.time.Months;
+import org.joda.time.Weeks;
 
 public class VacationTracker
 {
     private static final int WEEKS_IN_YEAR = 52;
+    private static final int MONTHS_IN_YEAR = 12;
     private static final int DAYS_IN_WEEK = 7;
     
     private LocalDate startDate;
     private float hoursUsed;
     private float hoursPerYear;
     private float initialHours;
+    private String leaveInterval;
     
     public VacationTracker() {
     }
-    public VacationTracker(LocalDate startDate, float hoursUsed, float hoursPerYear, float initialHours) {
+    public VacationTracker(LocalDate startDate, float hoursUsed, float hoursPerYear, float initialHours, String leaveInterval) {
         this.startDate = startDate;
         this.hoursUsed = hoursUsed;
         this.hoursPerYear = hoursPerYear;
         this.initialHours = initialHours;
+        this.leaveInterval = leaveInterval;
      }
     public float calculateHours(LocalDate asOfDate) {
-        Days days = Days.daysBetween(startDate, asOfDate);
-        float vacationHours = (days.getDays() * ((hoursPerYear / WEEKS_IN_YEAR) / DAYS_IN_WEEK) - hoursUsed);
+        int interval = 0;
+        float vacationHours = 0;
+        if (leaveInterval.equals("Day")) {
+            Days days = Days.daysBetween(startDate, asOfDate);
+            interval = days.getDays();
+            vacationHours = (interval * ((hoursPerYear / WEEKS_IN_YEAR) / DAYS_IN_WEEK) - hoursUsed);
+        } else if (leaveInterval.equals("Week")) {
+            Weeks weeks = Weeks.weeksBetween(startDate, asOfDate);
+            interval = weeks.getWeeks();
+            vacationHours = (interval * (hoursPerYear / WEEKS_IN_YEAR) - hoursUsed);
+        } else if (leaveInterval.equals("Month")) {
+            Months months = Months.monthsBetween(startDate, asOfDate);
+            interval = months.getMonths();
+            vacationHours = (interval * (hoursPerYear / MONTHS_IN_YEAR) - hoursUsed);
+        }
         return vacationHours;
     }
     public void setStartDate(LocalDate startDate)
